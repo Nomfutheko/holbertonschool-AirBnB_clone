f60e90cefdc28f2426bf701f63e68991bb9c6b93
Nomfutheko <thekothandiwe@gmail.com>
2022-03-23 08:48:28 +0200

    Update 5-substraction.c

--- 0x00-math_complex/5-substraction.c
+++ 0x00-math_complex/5-substraction.c
@@ -1,14 +1,11 @@
 #include "holberton.h"
 #include <math.h>
-
 /**
-* substraction - return the substraction
-* @c1: complex
-* @c2: complex
-* @c3: complex pointer
-* Return: void
-*/
-
+ * substraction - function substraction operation to complex numbers
+ * @c1: complex 1
+ * @c2: complex 2
+ * @c3: result in complex 3
+ */
 void substraction(complex c1, complex c2, complex *c3)
 {
 	c3->re = c1.re - c2.re;
