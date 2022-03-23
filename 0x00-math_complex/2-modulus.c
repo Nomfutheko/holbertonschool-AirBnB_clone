8632395067f3542ff044757f1a01d561ca538885
Nomfutheko <thekothandiwe@gmail.com>
2022-03-23 08:43:44 +0200

    Update 2-modulus.c

--- 0x00-math_complex/2-modulus.c
+++ 0x00-math_complex/2-modulus.c
@@ -1,2 +1,16 @@
-#include "main.h"
+#include "holberton.h"
+#include <math.h>
 
+/**
+* modulus - return the modulus
+* @c: structure complex
+* Return: c
+*/
+
+double modulus(complex c)
+{
+	double mod;
+
+	mod = sqrt(pow(c.re, 2) + pow(c.im, 2));
+	return (mod);
+}
